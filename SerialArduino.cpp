640588469dddcaceb8f4c1602f7b1fb80eaa986c
HumaSOFT <humasoft@ing.uc3m.es>
2019-01-16 17:54:04 +0100

    modificado:    SerialArduino.cpp

--- SerialArduino.cpp
+++ SerialArduino.cpp
@@ -41,7 +41,7 @@ SerialArduino::SerialArduino()
             // open and configure the serialport
             port->setPortName(arduino_port_name);
             port->open(QIODevice::ReadWrite);
-            port->setBaudRate(QSerialPort::Baud9600);
+            port->setBaudRate(QSerialPort::Baud38400);
             port->setDataBits(QSerialPort::Data8);
             port->setParity(QSerialPort::NoParity);
             port->setStopBits(QSerialPort::OneStop);
